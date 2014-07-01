0eaa66eecef48aa931ddeb68d1349bc29178426a
Cristtopher Quintana Toledo <cristtopher@debian.cl>
2014-07-01 14:57:11 -0400

    Update skype4py.py

--- skype4py.py
+++ skype4py.py
@@ -1,125 +1,124 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import time
 import Skype4Py
 import random
 import logging
 import sqlite3
-import smtplib
-from email.mime.text import MIMEText
-# import base64
-# from Examples.EchoClient import WhatsappEchoClient 
+import random
+import base64
+from Examples.EchoClient import WhatsappEchoClient 
 
 
 def commands(Message, Status):
-    if Status == 'SENT' or (Status == 'RECEIVED'): 
-        heuristic(Message.Id, Message.FromHandle, Message.Body, Message.Datetime)
-        logging.info(Message.Body)
-        if Message.Body == "!?":
-            alive(Message)
-        elif "!ip" in Message.Body:
-            searchip(Message)
-        elif "!ping" in Message.Body:
-            ping(Message)
-        elif "buenos dias" in Message.Body:
-            greet(Message)
-        elif "hola" in Message.Body:
-            greet()
+    exist = False
+    #if Status == 'SENT' or Status == 'RECEIVED':
+    if Status == 'RECEIVED':
+        memory = sqlite3.connect(Path + 'memory.db')
+        cursor = memory.cursor()
+        cursor.execute("SELECT name FROM messages")
+        for row in cursor:
+            if row[0] == Message.ChatName:
+                exist = True
+            else:
+                exist = False
+        if exist == False:
+            cursor.execute("insert into messages values ('%s','%s','%s','%s')" % (Message.ChatName, Message.FromHandle, Message.Body, Message.Datetime))
+            memory.commit()
+            knowledge(Message.ChatName, Message.FromHandle, Message.Body, Message.Datetime)
+            sendwhatapp(Message.FromHandle, Message.Body)
+            logging.info(Message.Body)
+            if "ola" in Message.Body:
+                greet(Message)
+            elif "uenas" in Message.Body:
+                greet(Message)
+            else:
+                pass
         else:
-            pass
+            exist = False
+        memory.close()
     else:
         pass
 
 
-def alive(Message):
-    Message.Chat.Sendsendwhatapp()
-    print "Ping Command Received \n"
+def greet(Message):
+    time.sleep(3) #more human
+    if time.strftime("%p") == "AM":
+        timeis = "buen día"
+    else:
+        timeis = "buenas tardes"
+    responses1 = {1: "Hola %s" % (timeis), 
+                2: timeis,
+                3: "que tal",
+                4: "Hola",
+                5: "Hola estimado",
+                6: "Hola como te va?"}
+    responses2 = {1: "¿en qué te puedo ayudar?", 
+                2: "cuentame, como te ayudo?",
+                3: "como te ayudo?",
+                4: "dime, en que te puedo ayudar?",
+                5: "dime, como te ayudo?",
+                6: "cuentame"}
+    response = random.randrange(1, 6)
+    Message.Chat.SendMessage(responses1[response])
+    time.sleep(2) #more human
+    Message.Chat.SendMessage(responses2[response])
+    print "Greeting Received from %s!.\n" % (Message.FromHandle)
 
 
-def ping(Message):
-    Message.Chat.SendMessage('Centro Online')
-    print "Ping Command Received \n"
-
-
-def greet():
-    #Message.Chat.SendMessage('Hola buenas, Cristtopher por aca, Jefe Soporte Tecnico')
-    Message.Chat.SendMessage('Hola, que tal')
-    #Message.Chat.SendMessage('en que te puedo ayudar?')
-    print "Greeting Received!.\n"
-    sendwhatapp('Greeting Received!.')
+def heuristic():
+    # retomar despues de unos meses para que base de conocimientos se llene
+    pass
     
 
-
-def cmd_dice(Message):
-    Message.Chat.SendMessage('Robot: Put a bet on numbers 1 through 6.')
-    time.sleep(8)
-    answer = random.randint(1,6)
-    Message.Chat.SendMessage('Robot: *rolls dice*')
-    time.sleep(1)
-    Message.Chat.SendMessage('Robot: The dice rolled the number:')
-    Message.Chat.SendMessage(answer)
-    print "Someone's playing dice. \n"
-
-
-def heuristic(Id, User, Message, Date):
+def knowledge(Name, User, Message, Date):
     try:
-        if User != 'cristtopher_quintana':
-            sendmail(User, Message)
-            sendwhatapp(Message)
-            connection = sqlite3.connect(Path + 'memory.db')
-            cursor = connection.cursor()
-            cursor.execute("insert into messages(id, user, body, date) values (%i,'%s','%s','%s')" % (Id, User, Message, Date))
+        exist = False
+        connection = sqlite3.connect(Path + 'knowledge.db')
+        cursor = connection.cursor()
+        cursor.execute("SELECT name, body FROM messages")
+        for row in cursor:
+            if row[0] == Name and row[1] == Message:
+                exist = True
+            else:
+                exist = False
+        if exist is False:
+            cursor.execute("insert into messages values ('%s','%s','%s','%s')" % (Name, User, Message, Date))
+            print "Message remembered in knowledge"
+            logging.info("Message remembered in knowledge")
             connection.commit()
-            connection.close()
+        connection.close()
     except sqlite3.Error as e:
         print "An error occurred:", e.args[0]
         logging.error(e.args[0])
 
 
-def searchip(Message):
-    print "IP Command Received \n"
-    Message.Chat.SendMessage('la ip es: ')
-
-
-def sendmail(User, Message):
-    # Creamos el mensaje
-    text = User + ' -> ' +  Message
-    msg = MIMEText(text)
-    msg['Subject'] = 'Skype'
-    msg['From'] = 'cquintana@innovex.cl'
-    msg['To'] = 'cquintana@innovex.cl'
-    mailServer = smtplib.SMTP('smtp.1und1.de',587)
-    mailServer.ehlo()
-    mailServer.starttls()
-    mailServer.ehlo()
-    mailServer.login("cquintana@innovex.cl","xxxx")
-    mailServer.sendmail("cquintana@innovex.cl", "cquintana@innovex.cl", msg.as_string())
-    mailServer.close()
-
-
-def sendwhatapp(Message):
-    password = "cpL+ocV0i+V65/L0uO/7pR9XJLU="                      #Password dada al registrar el numero.
+def sendwhatapp(User, Message):
+    password = "xx"                      #Password dada al registrar el numero.
     password = base64.b64decode(bytes(password.encode('utf-8')))   #Codificacion de Password para envio a los servidores de whatsApp.
-    username = '56962365854'                                       #Numero de telefono para el inicio de secion.
-    keepAlive= False                                               #Conexion persistente con el servidor.
-    #......................................................................
-    whats = WhatsappEchoClient("+56962365854", Message, keepAlive)     #Inicia el cliente para el envio de mensajes por WhatsApp.
-    whats.login(username, password)  
+    username = 'xx'                                       #Numero de telefono para el inicio de secion.
+    keepAlive= False          
+    Text = "%s -> %s" %(str(User), str(Message))                            #Conexion persistente con el servidor.
+    whats = WhatsappEchoClient("xx", Text, keepAlive)     #Inicia el cliente para el envio de mensajes por WhatsApp.
+    whats2 = WhatsappEchoClient("xx", Text, keepAlive)     #Inicia el cliente para el envio de mensajes por WhatsApp.
+    print "Message sent to whatsApp"
+    logging.info("Message sent to whatsApp")
+    whats.login(username, password)
+    whats2.login(username, password)
 
+if __name__ == "__main__":
+    skype = Skype4Py.Skype()
+    skype.OnMessageStatus = commands
+    if skype.Client.IsRunning == False:
+        skype.Client.Start()
+    skype.Attach();
+    Path = '/home/cristtopher/.Skype/'
+    print 'This Bot currently running on user', skype.CurrentUser.FullName,\
+     "(" + skype.CurrentUserHandle + ")", "\n"
+    logging.basicConfig(filename=Path + 'Skype.log',level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logging.info('Started')
+    while True:
+        raw_input('')
 
-print "|----------------------------------------------|\n";
-print "|               Welcome To InnoBot             |\n";
-print "|----------------------------------------------|\n";
-skype = Skype4Py.Skype(); 
-skype.OnMessageStatus = commands 
-
-if skype.Client.IsRunning == False: 
-    skype.Client.Start() 
-skype.Attach();
-Path = '/home/cristtopher/.Skype/'
-print 'InnoBot currently running on user',skype.CurrentUserHandle, "\n"
-logging.basicConfig(filename=Path + 'Skype.log',level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-logging.info('Started')
-while True: 
-    raw_input('')
